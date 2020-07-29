b99eb4ca9369d02a63761e0f0ae1eb2f7f47b71d
Lukaribou <lucasperroud17@gmail.com>
2020-07-29 20:19:56 +0200

    Commande d'aide

--- commands.go
+++ commands.go
@@ -1,6 +1,27 @@
 package main
 
-func pingCommand(ctx Context) error {
+import "github.com/bwmarrin/discordgo"
+
+func pingCommand(ctx Context) {
 	ctx.Reply("Pong !")
-	return nil
+}
+
+func helpCommand(ctx Context) {
+	var fields []*discordgo.MessageEmbedField
+	for _, c := range Config.CommandHandler.Commands {
+		fields = append(fields, &discordgo.MessageEmbedField{
+			Name:   c.Name,
+			Value:  c.Description,
+			Inline: true,
+		})
+	}
+
+	em := &discordgo.MessageEmbed{
+		Author: &discordgo.MessageEmbedAuthor{
+			Name:    ctx.Session.State.User.Username + " - Page d'aide",
+			IconURL: ctx.Session.State.User.AvatarURL(""),
+			URL:     Config.GitHubLink},
+		Fields: fields,
+	}
+	ctx.Session.ChannelMessageSendEmbed(ctx.Channel.ID, em)
 }
