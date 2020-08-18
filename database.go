c12c51a181f55f02bca4a2aaf00171e295ad9539
Lukaribou <lucasperroud17@gmail.com>
2020-08-18 23:30:29 +0200

    GetCurrentLanguage

--- database.go
+++ database.go
@@ -33,11 +33,16 @@ func (db *DB) DbConnect() *sql.DB {
 	return d
 }
 
+// ExecExistWithQuery : ...
+func (db *DB) ExecExistWithQuery(tmpl string, el ...interface{}) bool {
+	var u string
+	e := db.sql.QueryRow(tmpl, el...).Scan(&u)
+	return e == nil
+}
+
 // PlayerExist : Renvoie true si l'id se trouve dans la bdd
 func (db *DB) PlayerExist(userID string) bool {
-	var u string
-	e := db.sql.QueryRow("SELECT ID FROM users WHERE userID = ?", userID).Scan(&u)
-	return e != sql.ErrNoRows && e == nil
+	return db.ExecExistWithQuery("SELECT ID FROM users WHERE userID = ?", userID)
 }
 
 // GetPlayer : Renvoie l'utilisateur si il existe, une erreur sinon
@@ -52,7 +57,28 @@ func (db *DB) GetPlayer(userID string) (*Player, error) {
 		&pl.money,
 		&pl.level,
 		&pl.createDate,
-		&pl.lastCode)
+		&pl.lastCode,
+		&pl.skills)
 
 	return &pl, nil
+}
+
+// ExistLanguage : Retourne true si le language est dans la BDD
+func (db *DB) ExistLanguage(name string) bool {
+	return db.ExecExistWithQuery("SELECT ID FROM langs WHERE name = ?", name)
+}
+
+// GetCurrentLanguage : Prends le langage correspondant à l'ID donné
+func (db *DB) GetCurrentLanguage(curLangName string) (*Language, error) {
+	if !db.ExecExistWithQuery("SELECT ID FROM langs WHERE name = ?", curLangName) {
+		return nil, errors.New("Le '" + curLangName + "' ne correspond à aucun langage.")
+	}
+	var l Language
+	db.sql.QueryRow("SELECT * FROM langs WHERE name = ?", curLangName).Scan(
+		&l.ID,
+		&l.name,
+		&l.level,
+		&l.skills)
+
+	return &l, nil
 }
